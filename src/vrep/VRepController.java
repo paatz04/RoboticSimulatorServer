b340687035babca2dce20e3947f60bfae8d7e2df
JoseLuisAcv2 <joseluisacevedo1995@gmail.com>
2018-01-10 21:03:16 +0100

    VRepController file: initialization and arm controller functions

--- src/vrep/VRepController.java
+++ src/vrep/VRepController.java
@@ -1,5 +1,7 @@
 package vrep;
 
+import coppelia.*;
+
 /* ToDo
  * This class should communicate with the vRep and control the vRep. With this class an external class should be able
  * to move the robotic arm, with using only the functions of this class.
@@ -12,18 +14,37 @@ public class VRepController extends Thread {
      *   set speed for the tip = 3 -> move the tip of the robotic arm every 20 milliseconds per 3 units up
      */
     private final static int UPDATE_FREQUENCY_MILLI_SECONDS = 20;
+    private final static String SIMULATOR_SCENE = "RoboticArmSimulator";
+
+    private remoteApi mVrep;
+    private int mClientID;
 
     public VRepController() {
-        // ToDo
+        mVrep = new remoteApi();
     }
 
-
     public void stopVRepController() {
-        // ToDo
+        mVrep.simxFinish(mClientID);
     }
 
     public void run() {
-        // ToDo
+        connectToServer();
+        if (isConnectedToServer()) {
+            System.out.println("Connected to V-REP API server...");
+        } else {
+            System.out.println("Failed to connect to V-REP API server...");
+            System.exit(1);
+        }
+    }
+
+    private void connectToServer() {
+        mVrep.simxFinish(-1);
+        mClientID = mVrep.simxStart("127.0.0.1",19999,true,
+                                    true,5000,5);
+    }
+
+    private boolean isConnectedToServer() {
+        return mVrep.simxGetConnectionId(mClientID) != -1;
     }
 
     /**
@@ -35,7 +56,15 @@ public class VRepController extends Thread {
      */
     public void setSpeedGrab(double speed) {
         System.out.println("VREP.GRAB\t" + speed);
-        // ToDo
+        FloatWA inFloats = new FloatWA(1);
+        inFloats.getArray()[0] = (float) speed;
+        int result=mVrep.simxCallScriptFunction(mClientID,SIMULATOR_SCENE,mVrep.sim_scripttype_childscript,
+                                                "setSpeedGrab_function",null,inFloats,
+                                                null,null,null,null,null,
+                                                null,mVrep.simx_opmode_blocking);
+        if (result != mVrep.simx_return_ok) {
+            System.out.format("VREP.GRAB remote function call failed\n");
+        }
     }
 
     /**
@@ -43,13 +72,19 @@ public class VRepController extends Thread {
      * positive value = tip up
      * negative value = tip down
      *
-     * Example: setSpeedTip(-3) -> the tip of the robotic arm should move with a speed of 3 units down.
-     *
      * @param speed
      */
     public void setSpeedTip(double speed) {
         System.out.println("VREP.TIP\t" + speed);
-        // ToDo
+        FloatWA inFloats = new FloatWA(1);
+        inFloats.getArray()[0] = (float) speed;
+        int result=mVrep.simxCallScriptFunction(mClientID,SIMULATOR_SCENE,mVrep.sim_scripttype_childscript,
+                                                "setSpeedTip_function",null,inFloats,
+                                                null,null,null,null,null,
+                                                null,mVrep.simx_opmode_blocking);
+        if (result != mVrep.simx_return_ok) {
+            System.out.format("VREP.TIP remote function call failed\n");
+        }
     }
 
     /**
@@ -61,7 +96,15 @@ public class VRepController extends Thread {
      */
     public void setSpeedBody(double speed) {
         System.out.println("VREP.BODY\t" + speed);
-        // ToDo
+        FloatWA inFloats = new FloatWA(1);
+        inFloats.getArray()[0] = (float) speed;
+        int result=mVrep.simxCallScriptFunction(mClientID,SIMULATOR_SCENE,mVrep.sim_scripttype_childscript,
+                                                "setSpeedBody_function",null,inFloats,
+                                                null,null,null,null,null,
+                                                null,mVrep.simx_opmode_blocking);
+        if (result != mVrep.simx_return_ok) {
+            System.out.format("VREP.BODY remote function call failed\n");
+        }
     }
 
     /**
@@ -73,6 +116,14 @@ public class VRepController extends Thread {
      */
     public void setSpeedRotation(double speed) {
         System.out.println("VREP.ROTATION\t" + speed);
-        // ToDo
+        FloatWA inFloats = new FloatWA(1);
+        inFloats.getArray()[0] = (float) speed;
+        int result=mVrep.simxCallScriptFunction(mClientID,SIMULATOR_SCENE,mVrep.sim_scripttype_childscript,
+                                                "setSpeedRotation_function",null,inFloats,
+                                                null,null,null,null,null,
+                                                null,mVrep.simx_opmode_blocking);
+        if (result != mVrep.simx_return_ok) {
+            System.out.format("VREP.ROTATION remote function call failed\n");
+        }
     }
 }
